7b5ad96e6700d69a1357db9da5001d5b1bcc8534
Francisco Mestizo <A01731549@tec.mx>
2023-04-12 17:20:07 -0600

    Creacion de la rama frontend

---
+++ app/routes/tests.tsx
@@ -0,0 +1,15 @@
+export default function TestsPages() {
+    return(
+        <>
+            <div className="flex flex-row">
+                <div className="basis-1/2">
+                    <p>Evaluaciones</p>
+                    <p>Aqui va el componente de la tabla</p>
+                </div>
+                <div className="basis-1/2">
+                    <p>Puntaje Máximo: *prop de puntaje maximo*</p>
+                </div>
+            </div>
+        </>
+    )
+}
