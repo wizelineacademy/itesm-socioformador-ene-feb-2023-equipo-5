dae0485404dde2852f211900f2a648cfafd65538
mcSalmi <mcSalmi>
2023-04-13 03:17:25 -0600

    grammar section

--- app/components/Video.js
+++ app/components/Video.js
@@ -2,6 +2,7 @@ import React from "react";
 import video from "./video.png";
 import IA from "app/components/IA.js";
 import Progress from "./Progress";
+import Section from "./Section";
 
 function Video(props) {
   return (
@@ -10,7 +11,10 @@ function Video(props) {
         <img src={video} alt={props.alt} className=" w-3/5 h-auto" />
         <IA></IA>
       </div>
-      <Progress checked={true} />
+      <div className="bg-white rounded-lg p-4 my-10 flex items-center">
+        <Progress checked={true} />
+        <Section texto="Grammar section"></Section>
+      </div>
     </div>
   );
 }
