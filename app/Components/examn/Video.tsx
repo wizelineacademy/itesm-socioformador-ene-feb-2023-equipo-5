48943692f7df8e85f667c386e32d8f6541c185f7
mcSalmi <mcSalmi>
2023-04-19 14:40:45 -0600

    ya funciona la descarga de video

--- app/Components/examn/Video.tsx
+++ app/Components/examn/Video.tsx
@@ -54,7 +54,7 @@ function Video(props: any) {
       document.body.appendChild(a);
       a.style.display = "none";
       a.href = url;
-      a.download = "react-webcam-stream-capture.mp4";
+      a.download = "react-webcam-stream-capture.webm";
       a.click();
       window.URL.revokeObjectURL(url);
       setRecordedChunks([]);
