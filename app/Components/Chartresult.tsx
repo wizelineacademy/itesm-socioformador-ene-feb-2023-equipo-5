fe048bcf0e2620d85b238dc3e65e16ba99f53441
Victoria Lucero R <70721302+VictoriaLR01@users.noreply.github.com>
2023-04-24 12:22:07 -0600

    Chart
    template básico del chart, falta agregar números laterales

--- app/Components/Chartresult.tsx
+++ app/Components/Chartresult.tsx
@@ -4,38 +4,36 @@ function Chart() {
 
      
         <div className="flex flex-col items-center w-full max-w-screen-md p-6 pb-6 bg-white rounded-lg shadow-xl sm:p-8">
-            <h2 className="text-xl font-bold">Monthly Revenue</h2>
-            <span className="text-sm font-semibold text-gray-500">2020</span>
-            <div className="flex items-end flex-grow w-full mt-2 space-x-2 sm:space-x-3">
-                <div className="relative flex flex-col items-center flex-grow pb-5 group">
-                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$37,500</span>
-                    <div className="relative flex justify-center w-full h-10 bg-graybgfigma"></div>
-                    <div className="relative flex justify-center w-full h-96 bg-bluefigma1"></div>
+            <h2 className="text-xl font-bold">Resultados</h2>
+            <div className="flex items-end flex-grow w-full mt-2 space-x-2 sm:space-x-0">
+                 <div className="relative flex flex-col items-center flex-grow pb-5 group">
+                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block ">96%</span>
+                    <div className="relative flex justify-center w-16 h-96 z-10 rounded-lg bg-bluefigma6"></div>
                     <span className="absolute bottom-0 text-xs font-bold">Lectura</span>
                 </div>
                 <div className="relative flex flex-col items-center flex-grow pb-5 group">
-                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$45,000</span>
-                    <div className="relative flex justify-center w-full h-36 bg-bluefigma2"></div>
+                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">30%</span>
+                    <div className="relative flex justify-center w-16 h-36 rounded-lg bg-bluefigma5"></div>
                     <span className="absolute bottom-0 text-xs font-bold">Escritura</span>
                 </div>
                 <div className="relative flex flex-col items-center flex-grow pb-5 group">
-                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$47,500</span>
-                    <div className="relative flex justify-center w-full h-24 bg-bluefigma3"></div>
+                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">50%</span>
+                    <div className="relative flex justify-center w-16 h-52 rounded-lg bg-bluefigma4"></div>
                     <span className="absolute bottom-0 text-xs font-bold">Escuchar</span>
                 </div>
                 <div className="relative flex flex-col items-center flex-grow pb-5 group">
-                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$50,000</span>
-                    <div className="relative flex justify-center w-full h-10 bg-bluefigma4"></div>
+                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">12%</span>
+                    <div className="relative flex justify-center w-16 h-12 rounded-lg bg-bluefigma3"></div>
                     <span className="absolute bottom-0 text-xs font-bold">Gramática</span>
                 </div>
                 <div className="relative flex flex-col items-center flex-grow pb-5 group">
-                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$47,500</span>
-                    <div className="relative flex justify-center w-full h-20 bg-bluefigma5"></div>
+                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">80%</span>
+                    <div className="relative flex justify-center w-16 h-80 rounded-lg bg-bluefigma2"></div>
                     <span className="absolute bottom-0 text-xs font-bold">Dicción</span>
                 </div>
                 <div className="relative flex flex-col items-center flex-grow pb-5 group">
-                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">$55,000</span>
-                    <div className="relative flex justify-center w-full h-16 bg-bluefigma6"></div>
+                    <span className="absolute top-0 hidden -mt-6 text-xs font-bold group-hover:block">40%</span>
+                    <div className="relative flex justify-center w-16 h-40 rounded-lg bg-bluefigma1"></div>
                     <span className="absolute bottom-0 text-xs font-bold">Vocabulario</span>
                 </div>
             </div>
