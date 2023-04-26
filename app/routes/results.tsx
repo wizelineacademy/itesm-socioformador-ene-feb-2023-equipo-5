c7759af8ac12fa842d3ce8a25a3f6e9c6f004ba6
Ariadna125 <88726891+Ariadna125@users.noreply.github.com>
2023-04-26 16:07:10 -0600

    Agregamos nuevas pantallas

--- app/routes/results.tsx
+++ app/routes/results.tsx
@@ -10,18 +10,23 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Result() {
   return (
-    <div>
-      <canvas className="max-w-100" id="TrafficChart"></canvas>
-      <h1 className="text-grayfigma">Welcome to Result</h1>
-      <Chart />
-      <Table />
-
-      <SquareR />
-      <Link to="/Instructions">
-        <button className="flex flex-row mt-9 mx-10 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
-          Repetir prueba
-        </button>
-      </Link>
+    <div className="grid grid-cols-4 grid-flow-col gap-10">
+      <div>
+        <canvas className="max-w-100" id="TrafficChart"></canvas>
+        <h1 className="text-grayfigma">Welcome to Result</h1>
+        <Table />
+      </div>
+      <div>
+        <Chart />
+        <SquareR />
+      </div>
+      {/* <div>
+        <Link to="/Instructions">
+          <button className="flex flex-row mt-9 mx-10 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+            Repetir prueba
+          </button>
+        </Link>
+      </div> */}
     </div>
   );
 }
