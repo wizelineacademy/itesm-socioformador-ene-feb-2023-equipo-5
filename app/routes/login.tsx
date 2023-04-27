7e2e881a765444bbb0b0f82c0f60aeac3cebf718
Francisco Mestizo <A01731549@tec.mx>
2023-04-26 19:30:07 -0600

    Actualizacion de texto al login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -12,15 +12,14 @@ export default function LoginPage() {
         <>
             <div className="grid place-content-center">
                 <div className="max-w-lg grid place-content-center px-24 py-12 mt-20 border-2 border-stone-200 rounded-2xl shadow-xl">
-                    <p className="font-bold text-xl text-center">Inicia sesión</p>
-                    <p className="mt-6 text-justify text-gray-600">Bienvenido de nuevo a <span className="text-teal-400 font-bold">SmartSpeak</span>. Estamos encantados de 
-                        ayudarte en tu camino hacia la mejora de tus habilidades 
-                        en el idioma inglés. ¡Comencemos!
+                    <p className="font-bold text-xl text-center">Welcome!</p>
+                    <p className="mt-6 text-justify text-gray-600">Welcome back to <span className="text-bluefigma5 font-bold">SmartSpeak</span>. We are thrilled to assist you on your 
+                    journey towards improving your English language skills. 
+                    Let's get started!
                     </p>
-                    <Form action="/auth/auth0" method="post">
-                        <button className="mt-10 px-8 py-4 border-2 border-stone-200 rounded-lg shadow">
-                            <object data={googleIcon} className="h-8 inline-flex"/>
-                            <span className="ml-4">Continua con Google</span>
+                    <Form action="/auth/auth0" method="post" className="grid place-content-center">
+                        <button className="mt-10 px-20 py-4 border-2 border-stone-200 rounded-lg shadow bg-bluefigma5">
+                            <span className="text-cyan-50">Log in</span>
                         </button>
                     </Form>
                 </div>
