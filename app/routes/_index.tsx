5a9ce4926653ad2ae7d04d2c581045f9cca5d457
Ariadna125 <88726891+Ariadna125@users.noreply.github.com>
2023-04-26 20:53:08 -0600

    Modificamos las rutas, quedo listo el flujo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,64 +1,57 @@
-import React from "react";
-import brainWaveLogo from "../../public/img/LogoAzulSinFondo.png";
-import { Link } from "react-router-dom";
+import { Link } from "@remix-run/react";
 
-const HeaderPage = () => {
+// import homeStyles from '~/styles/home.css';
+
+//import Header from "~/Components/Header";
+
+import Main from "~/Components/Main";
+import AboutSection from "~/Components/AboutSection";
+import ValuesSection from "~/Components/ValuesSection";
+import SubscribeSection from "~/Components/SubscribeSection";
+import Footer from "~/Components/Footer";
+import FirstSection from "~/Components/FirstSection";
+import HeaderPage from "~/Components/HeaderPage";
+
+export default function Index(): JSX.Element {
   return (
-    // <div classNameName='mx-auto w-64'>
-    //     <ul classNameName='text-sky-500 flex'>
-    //         <li classNameName='px-4 py-2 whitespace-nowrap'>About us</li>
-    //         <li classNameName='px-4 py-2 whitespace-nowrap'>Products</li>
-    //         <li classNameName='px-4 py-2 whitespace-nowrap'>Pricing</li>
-    //     </ul>
-    // </div>
-    <header className="sticky top-0 z-50 flex flex-row mt-0 mx-10 justify-between bg-gray-100 py-4 px-10">
-      <div>
-        <img
-          src={brainWaveLogo}
-          alt="Page Logo"
-          className="h-20 flex flex-row mt-5 mx-15 "
+    <main id="content">
+      <div className="bg-gray-100">
+        <Main />
+        <HeaderPage />
+        <FirstSection />
+        <AboutSection
+          title="Mision"
+          description="To offer a wide range of innovative software products by
+            implementing AI. To enable businesses to streamline their processes
+            by providing them with AI-based tools."
+          img_1_url="https://img.freepik.com/free-photo/ai-chip-artificial-intelligence-future-technology-innovation_53876-129780.jpg"
+          img_2_url="https://www.becas-santander.com/content/dam/becasmicrosites/blog/innovacion-tecnologica.jpg"
         />
+        <AboutSection
+          title="Vision"
+          description="To be a leading company in the market of intelligent and customized
+          web solutions. To use cutting-edge tools that allow us to bring
+          artificial intelligence technologies to the market."
+          img_1_url="https://img.freepik.com/vector-premium/concepto-tecnologia-comunicacion-mundo-digital_46706-848.jpg"
+          img_2_url="https://media.istockphoto.com/id/1164467533/photo/china-ningbo-cityscape.jpg?b=1&s=170667a&w=0&k=20&c=dVxzybt7A2_kQzWPDdqygSm0cOG8jh1cJVFl-rRkETg="
+        />
+        <ValuesSection />
+        <SubscribeSection />
+        <Footer />
       </div>
-      <nav className="flex flex-row mt-5 mx-15 items-center space-x-6 text-sky-600">
-        <a
-          href="#FirstSection"
-          className="hover:text-sky-800 hover:font-medium w-max"
-        >
-          Home
-        </a>
-
-        <a
-          href="#AboutSection"
-          className="hover:text-sky-800 hover:font-medium w-max"
-        >
-          About Us
-        </a>
-
-        <section>
-          <a
-            href="#SubscribeSection"
-            className="hover:text-sky-800 hover:font-medium w-max"
-          >
-            Contact
-          </a>
-        </section>
-
-        {/* <a
-          href="#SubscribeSection"
-          className="hover:text-sky-800 hover:font-medium w-max"
-        >
-          Contact
-        </a> */}
-      </nav>
-      <div>
-        <Link to="/login">
-          <button className="flex flex-row mt-9 mx-10 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
-            Log In
-          </button>
-        </Link>
-      </div>
-    </header>
+      {/* <h1>This is us</h1>
+      <p>Wizeline is facing the problem of receiving too many job applications and it needs to efficiently evaluate the applicants’ English level.</p>
+      <h2>Mision</h2>
+      <p>To offer a wide range of innovative software products by implementing AI. To enable businesses to streamline their processes by providing them with AI-based tools.</p>
+      <h2>Vision</h2>
+      <p>To be a leading company in the market of intelligent and customized web solutions. To use cutting-edge tools that allow us to bring artificial intelligence technologies to the market.</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p> */}
+    </main>
   );
-};
+}
 
-export default HeaderPage;
+// export function links(): {rel: string, href: string}[] {
+//   return [{ rel: 'stylesheet', href: homeStyles }];
+// }
