74b5729baef5d88a1f07fac65c00048b5bd0231c
Victoria Lucero R <70721302+VictoriaLR01@users.noreply.github.com>
2023-04-27 17:09:09 -0600

    PopUp completo

--- app/Components/DashboardAdmin.tsx
+++ app/Components/DashboardAdmin.tsx
@@ -3,15 +3,11 @@ import SquareR from "./SquareResult";
 function Dashboard() {
   return (
     <>
-      <div className="bg-gray-100">
-        <div className="flex justify-center">
-          <h2 className="text-xl font-bold ">RESULTADOS</h2>
-        </div>
-
-        <div className="w-2/4 float-left">
+      <div className="rounded-md ">
+        <div className="float-left  h-2/4 ">
           <Chart />
         </div>
-        <div className="w-1/4 mx-[10%] my-[2%] float-left">
+        <div className="float-right mx-5 h-2/5 ">
           <SquareR />
         </div>
       </div>
--- app/routes/resultsAdmin.tsx
+++ app/routes/resultsAdmin.tsx
@@ -1,31 +1,77 @@
-import ResultsTable from "~/components/ResultsTable"
+import ResultsTable from "~/components/ResultsTable";
 import SquareR from "~/components/SquareResult";
+import Chart from "~/Components/Chartresult";
+import Dashboard from "~/Components/DashboardAdmin";
+import React from "react";
 
 export default function Result() {
-    return (
-        <>
-            <div className="flex flex-row mt-14 mx-10">
-                <div className="pb-5 absolute inset-x-0 bottom-0">
-                    <div className="flex  place-content-between mx-8">
-                        <button className="py-2 w-40 rounded-md bg-blue-200">Cambiar resultados</button>
-                        <button className="py-2 w-40 rounded-md bg-graybgfigma">Recomendaciones</button>
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <div className="flex flex-row mt-14 mx-10">
+        <div className="pb-5 absolute inset-x-0 bottom-0">
+          <div className="flex  place-content-between mx-8">
+            <button className="py-2 w-40 rounded-md bg-blue-200">
+              Cambiar resultados
+            </button>
+            <button className="py-2 w-40 rounded-md bg-graybgfigma">
+              Recomendaciones
+            </button>
+          </div>
+        </div>
+        <div className="basis-1/2 mx-2 relative ">
+          <p className="text-lg font-bold mb-4  ">Evaluaciones</p>
+          <span className="h-1 w-full bg-red-600 lg:w-1/3"></span>
+          <ResultsTable />
+        </div>
+        <div className="basis-1/2 mx-2">
+          <div className="mx-8 mt-4 p-3 ">
+            <button
+              className=" ease-linear transition-all duration-150"
+              type="button"
+              onClick={() => setShowModal(true)}
+            >
+              <SquareR />
+            </button>
+            {showModal ? (
+              <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                  <div className="relative w-auto my-6 mx-auto max-w-6xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                      {/*header*/}
+                      <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                        <h3 className="text-2xl font-semibold">RESULTADOS</h3>
+                        <button
+                          className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                          onClick={() => setShowModal(false)}
+                        >
+                          <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                            ×
+                          </span>
+                        </button>
+                      </div>
+                      {/*body*/}
+                      <Dashboard />
+                      {/*footer*/}
+                      <div className="flex items-center justify-end px-6 py-2 border-t border-solid border-slate-200 rounded-b">
+                        <button
+                          className="bg-bluefigma5 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Cerrar
+                        </button>
+                      </div>
                     </div>
-
+                  </div>
                 </div>
-                <div className="basis-1/2 mx-2 relative ">
-
-                    <p className="text-lg font-bold mb-4  ">Evaluaciones</p>
-                    <span className="h-1 w-full bg-red-600 lg:w-1/3"></span>
-                    <ResultsTable />
-
-                </div>
-                <div className="basis-1/2 mx-2">
-                    <div className="mx-8 mt-4 p-3 ">
-                        <SquareR />
-                    </div>
-                </div>
-
-            </div>
-        </>
-    );
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
