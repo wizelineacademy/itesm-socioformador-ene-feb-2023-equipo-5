85b269ecdda6278e8b8d08004523afa0962c210b
Francisco Mestizo <A01731549@tec.mx>
2023-06-07 00:42:29 -0600

    Se arreglaron errores de merge en el header (login y nuevos props)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,49 +14,9 @@ function Header(props: any) {
           />
         </Link>
       </div>
-{/* <!-- <<<<<<< H101/Logout --> */}
-      <div className="inline-block basis-3/12">
-        <Link
-          to={
-            props.nombre._json["https://smartspeak.example.com/roles"].includes(
-              "admin"
-            )
-              ? "/admin/videos"
-              : "/user/profile"
-          }
-          className="inline-block basis-1/4"
-        > 
-          <img
-            src={profilePicture}
-            className="h-12 my-4 rounded-full float-left"
-            alt="Usuario"
-            referrerPolicy="no-referrer"
-          />
-          <span className="text-white text-base mx-5 my-7 float-left">
-            {props.nombre.givenName}
-          </span>
-        </Link>
-      </div>
-      <div className="inline-block basis-1/12">
-        <Form method="post" action="/auth/logout">
-          <button className="bg-bluefigma4 hover:bg-bluefigma6 w-full">
-            <span className="text-white text-base mx-10 my-7 float-left">
-              Logout
-            </span>
-          </button>
-        </Form>
-      </div>
-{/* <!-- =======
-      <Link
-        to={
-          props.nombre._json["https://smartspeak.example.com/roles"].includes(
-            "admin"
-          )
-            ? "/adminProfile"
-            : "/user/profile"
-        }
-        className="inline-block basis-1/4"
-      >
+      <Link to={
+        props.role == "admin" ? "/admin/videos" : "/user/profile"
+      } className="inline-block basis-3/12">
         <img
           src={profilePicture}
           className="h-12 my-4 rounded-full float-left"
@@ -67,7 +27,15 @@ function Header(props: any) {
           {props.name}
         </span>
       </Link>
-// >>>>>>> dev --> */}
+      <div className="inline-block basis-1/12">
+        <Form method="post" action="/auth/logout">
+          <button className="bg-bluefigma4 hover:bg-bluefigma6 w-full">
+            <span className="text-white text-base mx-10 my-7 float-left">
+              Logout
+            </span>
+          </button>
+        </Form>
+      </div>
     </div>
   );
 }
