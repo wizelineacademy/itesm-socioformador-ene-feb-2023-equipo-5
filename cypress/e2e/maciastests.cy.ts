7fc83d14547c79dcd409aff5e0a3a03d4a0eb10f
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2023-06-15 16:48:23 -0600

    Pruebas de Macias

--- cypress/e2e/maciastests.cy.ts
+++ cypress/e2e/maciastests.cy.ts
@@ -28,9 +28,160 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 // })
 // ESTA PRUEBA SI FUNCIONA EN LOCAL, PERO EN EL CI TRUENA
 
-describe('test', () => {
+//Prueba #1
+describe('Filtrado de Usuarios', () => {
     it('passes', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.get('select#dificultadusers').select('A2')
+    })
+})
+
+//Prueba #2
+describe('Dashboard de Usuarios', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.get('a#dashusers').click()
+    })
+})
+
+//Prueba #3
+describe('Informacion de un usuario', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.contains('td', 'prueba 11 prueba').click()
+    })
+})
+
+//Prueba #4
+describe('Busqueda de Usuarios', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.contains('li', 'Videos').click()
+        cy.get('input#searchvideo').type('prueba 10 prueba')
+    })
+})
+
+//Prueba #5
+describe('Detalles del examen de un usuario', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.contains('td', 'prueba 4 prueba').click()
+        cy.wait(5000)
+        cy.contains('Details').click()
+    })
+})
+
+//Prueba #6
+describe('Detalles del video grabado de un usuario', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.contains('li', 'Videos').click()
+        cy.get(':nth-child(1) > .underline > a').click()
+    })
+})
+
+//Prueba #7
+describe('Corroborar que el nivel sea el mismo de un video y del datatable de Videos', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.contains('li', 'Videos').click()
+        cy.get(':nth-child(3) > .underline > a').click()
+        cy.wait(5000)
+        cy.get('.text-green-600').should('have.text', 'B2')
+    })
+})
+
+//Prueba #8
+describe('Fecha de un examen tomado', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.contains('td', 'prueba 4 prueba').click()
+        cy.wait(5000)
+        cy.get('tbody > .text-center > :nth-child(2)').should('not.have.text', 'Date')
+    })
+})
+
+//Prueba #9
+describe('Log out exitoso', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.get('.bg-bluefigma4').click()
+        cy.url().should('eq', 'http://localhost:3000/')      
+    })
+})
+
+//Prueba #10
+describe('Ir a menu y corroborar que aun estamos loggeados', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.get('a > .h-20').click()
+        cy.contains('button', 'Home').should('not.have.text', 'Log In')
+    })
+})
+
+//Prueba E2E que si sirve en CI
+describe('Recorrido por la pagina desde ver el video de un usuario hasta dejar de estar loggeado', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000');
+        cy.loginAdmin()
+        cy.visit('http://localhost:3000');
+        cy.contains('button', 'Home').click()
+        cy.wait(5000)
+        cy.get('input#searchusers').type('Carolina Díaz Perales')
+        cy.get('.text-center > :nth-child(2) > a').click()
+        cy.wait(5000)
+        cy.contains('Details').click()
+        cy.wait(5000)
+        cy.contains('a', 'Go Back').click()
+        cy.wait(5000)
+        cy.contains('li', 'Videos').click()
+        cy.get('select#dificultadvideos').select('B2')
+        cy.get('input#searchvideo').type('Carolina Díaz Perales')
+        cy.get('.underline > a').click()
+        cy.wait(5000)
+        cy.contains('a', 'Go back').click()
+        cy.get('.bg-bluefigma4').click()
+        cy.url().should('eq', 'http://localhost:3000/')
     })
 })
 
