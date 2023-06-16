df69d703b1e36328fa54f70dfa1b7a4bd9540b5c
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2023-06-16 09:35:14 -0600

    Se quito el job de Cypress en el CI

--- cypress/e2e/maciastests.cy.ts
+++ cypress/e2e/maciastests.cy.ts
@@ -6,28 +6,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
 
-//E2E test
-// function getRandomNumber(min: number, max: number): number {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// describe('Register', () => {
-//     it('passes', () => {
-//         cy.visit('http://localhost:3000')
-//         cy.get('button#login').click()
-//         cy.get('button#login').click()
-//         const randomNumber = getRandomNumber(1, 1000);
-//         const sentArgs = { username: `prueba${randomNumber}@cypress.com`, password: 'Secretpass1!' }
-//         cy.origin('https://smartspeak.us.auth0.com', { args: sentArgs }, ({ username, password }) => {
-//             cy.contains("Sign up").click()
-//             cy.get('input#email').type(username)
-//             cy.get('input#password').type(password)
-//             cy.contains('button', 'Continue').click()
-//             cy.contains('button', 'Accept').click()
-//         })
-//     })
-// })
-// ESTA PRUEBA SI FUNCIONA EN LOCAL, PERO EN EL CI TRUENA
-
 //Prueba #1
 describe('Filtrado de Usuarios', () => {
     it('passes', () => {
@@ -156,6 +134,27 @@ describe('Ir a menu y corroborar que aun estamos loggeados', () => {
         cy.wait(5000)
         cy.get('a > .h-20').click()
         cy.contains('button', 'Home').should('not.have.text', 'Log In')
+    })
+})
+
+//E2E test
+function getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+describe('Register', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('button#login').click()
+        cy.get('button#login').click()
+        const randomNumber = getRandomNumber(1, 1000);
+        const sentArgs = { username: `prueba${randomNumber}@cypress.com`, password: 'Secretpass1!' }
+        cy.origin('https://smartspeak.us.auth0.com', { args: sentArgs }, ({ username, password }) => {
+            cy.contains("Sign up").click()
+            cy.get('input#email').type(username)
+            cy.get('input#password').type(password)
+            cy.contains('button', 'Continue').click()
+            cy.contains('button', 'Accept').click()
+        })
     })
 })
 
